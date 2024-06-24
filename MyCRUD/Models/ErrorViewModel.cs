735e269bec0bf625c950ea2b15784aaaf6b67c1b
Safi Ullah Abid <safiullah.abid@itt360.com>
2024-06-24 11:24:48 -0700

    Changes in Error View Model

--- MyCRUD/Models/ErrorViewModel.cs
+++ MyCRUD/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace MyCRUD.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public int Age { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
